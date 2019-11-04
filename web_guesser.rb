17d2df1666cd2fe42170ce0779df465337058b6e
Ian Kesler <ianmkesler@gmail.com>
2019-11-04 14:22:06 -0800

    I5: Guess limiting

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+@@guesses = 5
+
 set :secret_number => rand(101)
 
 def check_guess(guess)
@@ -10,13 +12,23 @@ def check_guess(guess)
     elsif guess < 0
             response += "low"   
     elsif guess == 0
-        response += "correct" 
+            response += "correct"
     end
     response
 end
 
+def reset_game
+    settings.secret_number = rand(101) 
+    @@guesses = 5
+end
+
 get '/' do
     guess = params["guess"].to_i
+    @@guesses -= 1
     response = check_guess(guess - settings.secret_number)
-    erb :index, :locals => {:number => settings.secret_number, :response => response}
+    if @@guesses == 0
+        response = "lost" unless response == "correct"
+    end
+    reset_game if response == "lost" || response == "correct"
+    erb :index, :locals => {:number => settings.secret_number, :response => response, :guesses => @@guesses} #needs to return this!
 end
